f574a15caa33aac3f5dccd5be49342a0e4ec161f
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-07-18 20:34:38 +0200

    ADD: normal JavaScript Logic to Components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
  import './ExpenseItem.css';
  
- function ExpenseItem({date, title, amount}) {
+ function ExpenseItem(props) {
+   const month = props.date.toLocaleString('en-US', { month: 'long' });
+   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+   const year = props.date.getFullYear();
 
    return (
      <div className="expense-item">
-       <div>{date.toISOString}</div>
+       <div>
+         <div>{month}</div>
+         <div>{year}</div>
+         <div>{day}</div>
+       </div>
        <div className="expense-item__description">
-         <h2>{title}</h2>
-         <div classname="expense-item__price">${amount}</div>
+         <h2>{props.title}</h2>
+         <div classname="expense-item__price">${props.amount}</div>
        </div>
      </div>
    );
