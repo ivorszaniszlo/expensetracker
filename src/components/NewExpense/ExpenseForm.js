8c8c835a0365f6d7202f55edae0edd514b808bb4
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-07-26 22:55:14 +0200

    UPDATE: state that depends on the previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -15,11 +15,16 @@ const ExpenseForm = () => {
 
   const titleChangeHandler = (event) => {
     //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    })
+    //setUserInput({
+    // ...userInput,
+    // enteredTitle: event.target.value,
+    //});
+    setUserInput((prevState) => {
+      return {...prevState, enteredTitle: event.target.value };
+    });
+
   };
+
 
   const amountChangeHandler = (event) => {
     //setEnteredAmount(event.target.value);
