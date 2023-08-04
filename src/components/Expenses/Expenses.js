a34a2607a3637bc8264ce509f07264f114641db5
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-08-04 15:09:33 +0200

    DELETE: date filter UPDATE: ExpenseItem from stateful component to stateless component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,7 +7,7 @@ import './Expenses.css';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
   
-  let filterInfoText = '2019, 2021 & 2022';
+  /*let filterInfoText = '2019, 2021 & 2022';
 
   if (filteredYear === '2019') {
     filterInfoText = '2020, 2021 & 2022';
@@ -15,7 +15,7 @@ const Expenses = (props) => {
     filterInfoText = '2019, 2020, 2022';
   } else {
     filterInfoText = '2019, 2020, 2021';
-  }
+  }*/
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -25,7 +25,6 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      <p>Data for years {filterInfoText} is hidden.</p>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
