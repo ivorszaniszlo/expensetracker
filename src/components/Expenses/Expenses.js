ed0f8707418f576dc289bdff93ea054e9a7faf5d
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-08-04 14:58:19 +0200

    ADD: Derived aka computed state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,15 +6,26 @@ import './Expenses.css';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
+  
+  let filterInfoText = '2019, 2021 & 2022';
 
-  const filterChangeHandler = selectedYear => {
+  if (filteredYear === '2019') {
+    filterInfoText = '2020, 2021 & 2022';
+  } else if (filteredYear === '2021') {
+    filterInfoText = '2019, 2020, 2022';
+  } else {
+    filterInfoText = '2019, 2020, 2021';
+  }
+
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  };
+  }
 
   return (
     <div>
       <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <p>Data for years {filterInfoText} is hidden.</p>
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
