8b54c99175b05ba700b83b8c3fe3a08ddd4fde80
ivorszaniszloRingier <szaniszlo.ivor@ringier.hu>
2023-08-06 16:00:56 +0200

    ADD: conditional content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
+    const startEditingHandler = () => {
+        setIsEditing(true);
+      };
+    
+      const stopEditingHandler = () => {
+        setIsEditing(false);
+      };
 
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
@@ -11,11 +20,15 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setIsEditing(false);
     };
 
-    return <div className='new-expense'>
-       <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
-    </div>
-};
-
-export default NewExpense;
+    return (
+        <div className='new-expense'>
+          {!isEditing && <button onClick={startEditingHandler}>Add New Expense</button>}
+          {isEditing && <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} onCancel={stopEditingHandler}/>}
+        </div>
+      );
+    };
+    
+    export default NewExpense;
